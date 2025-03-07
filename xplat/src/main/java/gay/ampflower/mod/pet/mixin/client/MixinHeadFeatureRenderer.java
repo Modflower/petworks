82f0893e4e3cad8969a74451db93d5603832ca09
Ampflower 🌺 <git@ampflower.gay>
2025-03-06 19:35:32 -0800

    Minimise function signature for head renderer suppressor

--- xplat/src/main/java/gay/ampflower/mod/pet/mixin/client/MixinHeadFeatureRenderer.java
+++ xplat/src/main/java/gay/ampflower/mod/pet/mixin/client/MixinHeadFeatureRenderer.java
@@ -1,17 +1,14 @@
 package gay.ampflower.mod.pet.mixin.client;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import gay.ampflower.mod.pet.client.HeadRendererSuppressor;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.HeadFeatureRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /**
  * @author Ampflower
@@ -21,10 +18,10 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Debug(export = true)
 public class MixinHeadFeatureRenderer {
 	@Inject(
-		method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V",
+		method = "render",
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z", shift = At.Shift.AFTER),
-		cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-	private void checkSuppressed(final MatrixStack matrixStack, final VertexConsumerProvider vertexConsumerProvider, final int i, final LivingEntity livingEntity, final float f, final float g, final float h, final float j, final float k, final float l, final CallbackInfo ci, ItemStack stack) {
+		cancellable = true)
+	private void checkSuppressed(final CallbackInfo ci, @Local final ItemStack stack) {
 		if (HeadRendererSuppressor.suppressed.contains(stack.getItem())) {
 			ci.cancel();
 		}
